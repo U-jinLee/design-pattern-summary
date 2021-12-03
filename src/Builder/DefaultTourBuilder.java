a2e3e8814eba58de81eed3845bb4d52fac147185
Yoojin Lee <yoojinLee.dev@gmail.com>
2021-12-03 22:44:37 +0900

    feat: DefaultTourBuilder.java commit

---
+++ src/Builder/DefaultTourBuilder.java
@@ -0,0 +1,14 @@
+package Builder;
+
+public class DefaultTourBuilder implements TourPlanBuilder{
+
+    @Override
+    public TourPlanBuilder title(String title) {
+        return null;
+    }
+
+    @Override
+    public TourPlanBuilder nightsAndDays(int nights, int days) {
+        return null;
+    }
+}
