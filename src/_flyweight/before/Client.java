c81bfd65f009b2afc8034ee1da916c74c1f46151
DESKTOP-D8U0NPS\USER-PC <leeujin1029@naver.com>
2023-01-25 17:43:30 +0900

    feat: Add flyweight before example

---
+++ src/_flyweight/before/Client.java
@@ -0,0 +1,11 @@
+package _flyweight.before;
+
+public class Client {
+    public static void main(String[] args) {
+        Character character1 = new Character('H', "white", "Nanum", 12);
+        Character character2 = new Character('e', "white", "Nanum", 12);
+        Character character3 = new Character('l', "white", "Nanum", 12);
+        Character character4 = new Character('l', "white", "Nanum", 12);
+        Character character5 = new Character('o', "white", "Nanum", 12);
+    }
+}
